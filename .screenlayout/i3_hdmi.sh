d5bafcceec8a208b1a069743a516544b45fc2c75
antoinemadec <ajamadec@gmail.com>
2017-05-10 13:45:42 -0700

    I3: screen names have changed after linux kernel and X server update
    We don't have any weird screen behavior anymore.

--- .screenlayout/i3_hdmi.sh
+++ .screenlayout/i3_hdmi.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1-2 --off --output DP1-1 --off
+xrandr --output eDP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --primary --output DP-1-2 --off --output DP-1-1 --off
