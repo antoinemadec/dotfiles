ffc83debc0603da62b58b12645286cbe338b8596
antoinemadec <ajamadec@gmail.com>
2018-03-15 16:44:19 -0700

    I3: better integration with 1 monitor only

--- .config/i3/bin/change_workspace.bash
+++ .config/i3/bin/change_workspace.bash
@@ -1,23 +1,36 @@
 #!/bin/bash
 set -e
 
-# this script assumes we are always using dual screen
+screen_nb="$(xrandr | grep ' connected ' | wc -l)"
 
 input="$1"
 focused_ws=$(i3-msg -t get_workspaces                   \
               | jq '.[] | select(.focused==true).num'   \
               | cut -d"\"" -f2)
-ws0=$(((focused_ws-1)/2*2 + 1))
-ws1=$((ws0+1))
 
-if [ "$input" = 0 ] && [ $ws0 -ge 3 ]
+# TODO: make it more elegant and generic
+if ((screen_nb == 1))
 then
-  ((ws0-=2))
-  ((ws1-=2))
-elif [ "$input" = 1 ] && [ $ws0 -le 7 ]
-then
-  ((ws0+=2))
-  ((ws1+=2))
+  ws0=$focused_ws
+  if [ "$input" = 0 ] && [ $ws0 -ge 2 ]
+  then
+    ((ws0-=1))
+  elif [ "$input" = 1 ] && [ $ws0 -le 9 ]
+  then
+    ((ws0+=1))
+  fi
+  i3-msg "workspace number $ws0"
+else
+  ws0=$(((focused_ws-1)/2*2 + 1))
+  ws1=$((ws0+1))
+  if [ "$input" = 0 ] && [ $ws0 -ge 3 ]
+  then
+    ((ws0-=2))
+    ((ws1-=2))
+  elif [ "$input" = 1 ] && [ $ws0 -le 7 ]
+  then
+    ((ws0+=2))
+    ((ws1+=2))
+  fi
+  i3-msg "workspace number $ws1; workspace number $ws0"
 fi
-
-i3-msg "workspace number $ws1; workspace number $ws0"
