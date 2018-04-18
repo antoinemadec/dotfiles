4e871478e2a7de38e4b72bdb565a1e9ee1d7a62b
antoinemadec <ajamadec@gmail.com>
2018-04-18 12:32:15 -0700

    I3 workspace: add rename with index 'r 4:foo'; add move 'm 9'

--- .config/i3/bin/jump_workspace.bash
+++ .config/i3/bin/jump_workspace.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# I- user input is <idx>:<name>
+# I- user input is <name>( <nb>)
+#
+#   create <nb> (default=1) workspaces called <first_available_idx>:<name>
+#
+#
+# II- user input is <idx>:<name>
 #
 #   if workspace <idx>:<name> already exists:
 #     jump into all workspaces mathing .*:<name>
@@ -11,16 +16,20 @@
 #       create workspace <idx>:<name>
 #
 #
-# II- user input is r(a) <new_name>
+# III- user input is r(a) (<idx>:)<new_name>
+#
 #   if ra:
 #     rename all workspaces matching .*:<focused_ws_name> in .*:<new_name>
 #   if r:
-#     rename <focused_ws> in <focused_ws_idx>:<new_name>
+#     if <idx> provided:
+#       rename <focused_ws> in <idx>:<new_name>
+#     else:
+#       rename <focused_ws> in <focused_ws_idx>:<new_name>
 #
 #
-# III- user input is <name>( <nb>)
+# IV- user input is m <idx>
 #
-#   create <nb> (default=1) workspaces called <first_available_idx>:<name>
+#   move <focused_ws> in <idx>:<focused_ws_name>
 
 
 get_workspaces()
@@ -73,8 +82,35 @@ find_first_available_ws()
 }
 
 ws="$(get_workspaces  | rofi -dmenu -p "workspace:")"
-
-if (echo "$ws" | grep -q ':')
+if (echo "$ws" | grep -q '^ra\? ')
+then
+  focused_ws="$(get_focused_workspace)"
+  focused_ws_name="$(echo "$focused_ws" | cut -d ':' -f 2)"
+  ws_new_name="$(echo "$ws" | cut -d ' ' -f 2)"
+  if (echo "$ws" | grep -q '^ra ')
+  then
+    match_list="$(get_workspaces | grep ":$focused_ws_name$" | sort -nr)"
+  else
+    match_list="$focused_ws"
+  fi
+  for w in $match_list
+  do
+    ws_idx="$(echo $w | cut -d ':' -f 1)"
+    i3-msg "workspace number $ws_idx"
+    if [[ "$ws_new_name" =~ ':' ]]
+    then
+      i3-msg "rename workspace to $ws_new_name"
+    else
+      i3-msg "rename workspace to $ws_idx:$ws_new_name"
+    fi
+  done
+elif (echo "$ws" | grep -q '^m ')
+then
+  focused_ws="$(get_focused_workspace)"
+  focused_ws_name="$(echo "$focused_ws" | cut -d ':' -f 2)"
+  ws_idx="$(echo "$ws"  | cut -d ' ' -f 2)"
+  i3-msg "rename workspace to $ws_idx:$focused_ws_name"
+elif (echo "$ws" | grep -q ':')
 then
   ws_idx="$(echo "$ws"  | cut -d ':' -f 1)"
   ws_name="$(echo "$ws" | cut -d ':' -f 2)"
@@ -90,23 +126,6 @@ then
     i3-msg "workspace number $ws_idx"
     i3-msg "rename workspace to $ws"
   fi
-elif (echo "$ws" | grep -q '^ra\? ')
-then
-  focused_ws="$(get_focused_workspace)"
-  focused_ws_name="$(echo "$focused_ws" | cut -d ':' -f 2)"
-  ws_new_name="$(echo "$ws" | cut -d ' ' -f 2)"
-  if (echo "$ws" | grep -q '^ra ')
-  then
-    match_list="$(get_workspaces | grep ":$focused_ws_name$" | sort -nr)"
-  else
-    match_list="$focused_ws"
-  fi
-  for w in $match_list
-  do
-    ws_idx="$(echo $w | cut -d ':' -f 1)"
-    i3-msg "workspace number $ws_idx"
-    i3-msg "rename workspace to $ws_idx:$ws_new_name"
-  done
 elif [ "$ws" != "" ]
 then
   ws_name="$(echo "$ws" | cut -d ' ' -f 1)"
