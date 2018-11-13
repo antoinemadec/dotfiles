ce002c37ac413c7ef2300b31eeb268e8b6d357ed
antoinemadec <ajamadec@gmail.com>
2018-11-12 16:50:15 -0800

    Wallpaper: add new ones, default is now a symlink

--- .config/i3/py3status/background.py
+++ .config/i3/py3status/background.py
@@ -4,11 +4,13 @@ Choose what backgound image to display
 
 import os
 
+
 class Py3status:
     cache_timeout = 2
     wallpaper_dir = os.path.expanduser('~/.config/i3/wallpaper')
     img_idx = 0
     full_text = 'bg'
+
     def rhythmbox(self):
         return {
                 'full_text': self.full_text,
@@ -16,9 +18,11 @@ class Py3status:
                 }
 
     def on_click(self, event):
-        button         = event['button']
-        wallpaper_list = os.listdir(self.wallpaper_dir)
+        button = event['button']
+        wallpaper_list = sorted([f for f in os.listdir(self.wallpaper_dir)
+                                 if f not in ("default", "standardize_names")])
+        print(wallpaper_list)
         next_wallpaper = wallpaper_list[self.img_idx]
         self.py3.command_run("feh --bg-scale %s" % os.path.join(self.wallpaper_dir, next_wallpaper))
         self.full_text = next_wallpaper.split('.')[0]
-        self.img_idx   = (self.img_idx + 1) % len(wallpaper_list)
+        self.img_idx = (self.img_idx + 1) % len(wallpaper_list)
