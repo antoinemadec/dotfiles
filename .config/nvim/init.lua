32ddc25a8a006766d61f85232ee04c97ec36b6bc
antoinemadec <ajamadec@gmail.com>
2021-11-20 20:23:46 +0100

    nvim: add bootstrap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,8 @@
 require('utils')
 require('plugins')
+if (packer_bootstrap) then
+  return
+end
 require('options')
 require('mappings')
 require('misc_vim')
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,4 +1,10 @@
--- plugins
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+
 require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -64,4 +70,9 @@ require('packer').startup(function(use)
   use 'tpope/vim-surround'                                      -- delete, change and add surroundings in pairs
   use 'tpope/vim-abolish'                                       -- work with variations of a word
   use 'antoinemadec/FixCursorHold.nvim'                         -- fix CursorHold perf bug
+
+  -- automatically set up your configuration after cloning packer.nvim
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
