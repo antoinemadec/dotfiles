953536aee71f69bf8d0375fefff9852cdaede450
antoinemadec <ajamadec@gmail.com>
2022-05-27 10:59:39 +0200

    nvim(lualine): fix after gruvbox_material update (2)

--- .config/nvim/lua/config/lualine.lua
+++ .config/nvim/lua/config/lualine.lua
@@ -1,5 +1,5 @@
 local gb_cfg = vim.fn['gruvbox_material#get_configuration']()
-local palette = vim.fn['gruvbox_material#get_palette'](gb_cfg.background, gb_cfg.palette, {dummy=0})
+local palette = vim.fn['gruvbox_material#get_palette'](gb_cfg.background, gb_cfg.foreground, {dummy=0})
 local ts_parsers = require "nvim-treesitter.parsers"
 
 
