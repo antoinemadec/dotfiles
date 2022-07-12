5299699074c3e6a138eff573165422efe4f08f8e
antoinemadec <ajamadec@gmail.com>
2022-07-12 16:53:40 +0200

    fix(fzf): alt+c was running aliased 'cd --'

--- .fzf.bash
+++ .fzf.bash
@@ -1,2 +1,10 @@
 source /usr/share/fzf/completion.bash
 source /usr/share/fzf/key-bindings.bash
+
+# override since cd -- already exists
+__fzf_cd__() {
+  local cmd dir
+  cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+    -o -type d -print 2> /dev/null | cut -b3-"}"
+  dir=$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" $(__fzfcmd) +m) && printf '\cd -- %q' "$dir"
+}
