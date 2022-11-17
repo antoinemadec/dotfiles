b7b8a8e517afbcd1fceaf8ac55afbe219a9d691e
antoinemadec <ajamadec@gmail.com>
2022-11-17 14:53:26 +0100

    feat(nvim): indent lines shows current context

--- .config/nvim/lua/config/indent_blankline.lua
+++ .config/nvim/lua/config/indent_blankline.lua
@@ -1,5 +1,6 @@
 require("indent_blankline").setup {
     buftype_exclude = {'help', 'terminal'},
-    filetype_exclude = {'startify', 'which_key', 'packer'},
+    filetype_exclude = {'startify', 'which_key', 'packer', 'mason'},
     show_trailing_blankline_indent = false,
+    show_current_context = true,
 }
