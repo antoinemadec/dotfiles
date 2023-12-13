b616d9f23d4d3185ec0f2c5362073cd2f32854be
antoinemadec <ajamadec@gmail.com>
2023-12-13 23:42:35 +0100

    fix: typo in mapping

--- .config/nvim/lua/avim/mappings.lua
+++ .config/nvim/lua/avim/mappings.lua
@@ -179,7 +179,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, opts)
     -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '\\rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '\\ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set({ 'n', 'v' }, '\\ac', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', require'telescope.builtin'.lsp_references, opts)
     vim.keymap.set('n', '\\f', function()
       vim.lsp.buf.format { async = true }
