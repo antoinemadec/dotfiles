8fd748b7744882652aa1302954af5a39db04b3f5
antoinemadec <ajamadec@gmail.com>
2024-02-23 16:18:39 +0100

    fix(nvim): tab issue with nvim-treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,7 @@
 vim.g.large_file_cutoff = 10000
 
 require('avim.utils')
-require('avim.plugins')
 require('avim.options')
+require('avim.plugins')
 require('avim.mappings')
 require('avim.misc_vim')
