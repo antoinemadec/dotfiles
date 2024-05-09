c9abc2785ff9e95b410d6c449887ce9a2d7c63de
antoinemadec <ajamadec@gmail.com>
2024-05-09 09:17:59 +0200

    feat(nvim): mappings for location and qf lists

--- .config/nvim/lua/avim/mappings.lua
+++ .config/nvim/lua/avim/mappings.lua
@@ -1,21 +1,31 @@
 -- functions
-local default_opts = { noremap = true, silent = true }
-local map_opts = { noremap = false, silent = true }
-local plug_opts = { noremap = false, silent = true }
-local remap = vim.keymap.set
 local t = _G.MUtils.t
 
-local function remap_arrow_hjkl(mode, lhs, rhs, opt)
+local function map(mode, lhs, rhs, opts)
+  -- silent by default
+  opts = opts or {}
+  opts.silent = opts.silent ~= false
+
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+local function map_arrow_hjkl(mode, lhs, rhs, opt)
   local arrow_hjkl_table = {
-    ['<Left>'] = 'h', ['<Down>'] = 'j', ['<Up>'] = 'k', ['<Right>'] = 'l',
-    Left = 'h', Down = 'j', Up = 'k', Right = 'l'
+    ['<Left>'] = 'h',
+    ['<Down>'] = 'j',
+    ['<Up>'] = 'k',
+    ['<Right>'] = 'l',
+    Left = 'h',
+    Down = 'j',
+    Up = 'k',
+    Right = 'l'
   }
   -- arrow mapping
-  remap(mode, lhs, rhs, opt)
+  map(mode, lhs, rhs, opt)
   -- hjkl mapping
   for arrow, hjkl in pairs(arrow_hjkl_table) do
     if string.find(lhs, arrow) then
-      remap(mode, string.gsub(lhs, arrow, hjkl), rhs, opt)
+      map(mode, string.gsub(lhs, arrow, hjkl), rhs, opt)
       return
     end
   end
@@ -31,15 +41,15 @@ end
 
 local function navigation_bar()
   if vim.bo.filetype == 'man' or vim.t.man_toc_win then
-    require('window-movement').toggle_side_bar("toc_win", function () require'man'.show_toc() end)
+    require('window-movement').toggle_side_bar("toc_win", function() require 'man'.show_toc() end)
   elseif vim.bo.filetype == 'help' or vim.t.help_toc_win then
-    require('window-movement').toggle_side_bar("toc_win", function () vim.cmd("normal gO") end)
+    require('window-movement').toggle_side_bar("toc_win", function() vim.cmd("normal gO") end)
   else
     require('avim.config.tagbar').toggle()
   end
 end
 
-local function next_location(direction)
+local function next_sidebar_location(direction)
   if vim.t.toc_win then
     if direction == 1 then
       vim.cmd("lnext")
@@ -76,7 +86,7 @@ local function mapping_func_key_help()
 <F6>      Highlight_1     add
 <F8>      Quickfix        toggle
 <F9>      Spell           toggle
-<F10>     Diff            toggle]],level, opts)
+<F10>     Diff            toggle]], level, opts)
   end
 end
 
@@ -85,92 +95,98 @@ vim.g.mapleader = t '<Space>'
 vim.g.maplocalleader = ','
 
 -- add multiple line movement to the jump list
-remap_arrow_hjkl('n', '<Down>', function() return smart_line_motion('j') end, { expr = true, noremap = true })
-remap_arrow_hjkl('n', '<Up>', function() return smart_line_motion('k') end, { expr = true, noremap = true })
+map_arrow_hjkl('n', '<Down>', function() return smart_line_motion('j') end, { expr = true, noremap = true })
+map_arrow_hjkl('n', '<Up>', function() return smart_line_motion('k') end, { expr = true, noremap = true })
 
 -- terminal escape
-remap('t', '<Esc><Esc>', function() return smart_term_escape() end, { expr = true, noremap = true })
+map('t', '<Esc><Esc>', function() return smart_term_escape() end, { expr = true, noremap = true })
 
 -- insert movement
-remap('i', '<C-h>', '<Left>', default_opts)
-remap('i', '<C-j>', '<Down>', map_opts)
-remap('i', '<C-k>', '<Up>', map_opts)
-remap('i', '<C-l>', '<Right>', default_opts)
+map('i', '<C-h>', '<Left>')
+map('i', '<C-j>', '<Down>', { remap = true })
+map('i', '<C-k>', '<Up>', { remap = true })
+map('i', '<C-l>', '<Right>')
 
 for _, mode in pairs({ 'n', 'i', 't' }) do
   local esc_chars = (mode == 'i' or mode == 't') and '<C-\\><C-n>' or ''
   -- window movement
-  remap_arrow_hjkl(mode, '<A-Left>', esc_chars .. '<C-w>h', default_opts)
-  remap_arrow_hjkl(mode, '<A-Down>', esc_chars .. '<C-w>j', default_opts)
-  remap_arrow_hjkl(mode, '<A-Up>', esc_chars .. '<C-w>k', default_opts)
-  remap_arrow_hjkl(mode, '<A-Right>', esc_chars .. '<C-w>l', default_opts)
-  remap_arrow_hjkl(mode, '<A-S-Left>', function() require('window-movement').move_win_to_direction("left") end, default_opts)
-  remap_arrow_hjkl(mode, '<A-S-Down>', function() require('window-movement').move_win_to_direction("down") end, default_opts)
-  remap_arrow_hjkl(mode, '<A-S-Up>', function() require('window-movement').move_win_to_direction("up") end, default_opts)
-  remap_arrow_hjkl(mode, '<A-S-Right>', function() require('window-movement').move_win_to_direction("right") end, default_opts)
-  remap_arrow_hjkl(mode, '<A-q>', function () require('window-movement').quad_win_cycle() end, default_opts)
+  map_arrow_hjkl(mode, '<A-Left>', esc_chars .. '<C-w>h')
+  map_arrow_hjkl(mode, '<A-Down>', esc_chars .. '<C-w>j')
+  map_arrow_hjkl(mode, '<A-Up>', esc_chars .. '<C-w>k')
+  map_arrow_hjkl(mode, '<A-Right>', esc_chars .. '<C-w>l')
+  map_arrow_hjkl(mode, '<A-S-Left>', function() require('window-movement').move_win_to_direction("left") end)
+  map_arrow_hjkl(mode, '<A-S-Down>', function() require('window-movement').move_win_to_direction("down") end)
+  map_arrow_hjkl(mode, '<A-S-Up>', function() require('window-movement').move_win_to_direction("up") end)
+  map_arrow_hjkl(mode, '<A-S-Right>', function() require('window-movement').move_win_to_direction("right") end)
+  map_arrow_hjkl(mode, '<A-q>', function() require('window-movement').quad_win_cycle() end)
   -- tab movement
-  remap_arrow_hjkl(mode, '<C-A-Left>', esc_chars .. 'gT', default_opts)
-  remap_arrow_hjkl(mode, '<C-A-Right>', esc_chars .. 'gt', default_opts)
-  remap_arrow_hjkl(mode, '<C-A-S-Left>', function() require('window-movement').move_win_to_tab("prev") end, default_opts)
-  remap_arrow_hjkl(mode, '<C-A-S-Right>', function() require('window-movement').move_win_to_tab("next") end, default_opts)
+  map_arrow_hjkl(mode, '<C-A-Left>', esc_chars .. 'gT')
+  map_arrow_hjkl(mode, '<C-A-Right>', esc_chars .. 'gt')
+  map_arrow_hjkl(mode, '<C-A-S-Left>', function() require('window-movement').move_win_to_tab("prev") end)
+  map_arrow_hjkl(mode, '<C-A-S-Right>', function() require('window-movement').move_win_to_tab("next") end)
 end
-remap_arrow_hjkl('n', '<C-Down>', '<C-e>', default_opts)
-remap_arrow_hjkl('n', '<C-Up>', '<C-y>', default_opts)
+map_arrow_hjkl('n', '<C-Down>', '<C-e>')
+map_arrow_hjkl('n', '<C-Up>', '<C-y>')
 
 -- tab split
-remap('n', '<C-w><C-t>', ':tab split<cr>', default_opts)
-remap('n', '<C-w>t', ':tab split<cr>', default_opts)
+map('n', '<C-w><C-t>', ':tab split<cr>')
+map('n', '<C-w>t', ':tab split<cr>')
 
 -- function keys
-remap('n', '<F1>', mapping_func_key_help, default_opts)
-remap('n', '<F2>', function() require("dapui").toggle() end, default_opts)
-remap('n', '<F3>', ':ToggleIndent<cr>', default_opts)
-remap('n', '<F4>', navigation_bar, default_opts)
-remap('n', '<F5>', ':exe "HighlightGroupsAddWord " . hg0 . " 1"<cr>', default_opts)
-remap('n', '\\<F5>', ':exe "HighlightGroupsClearGroup " . hg0 . " 1"<cr>', default_opts)
-remap('n', '<F6>', ':exe "HighlightGroupsAddWord " . hg1 . " 1"<cr>', default_opts)
-remap('n', '\\<F6>', ':exe "HighlightGroupsClearGroup " . hg1 . " 1"<cr>', default_opts)
-remap('n', '<F8>', ':call asyncrun#quickfix_toggle(8)<cr>', default_opts)
-remap('n', '<F9>', ':set spell!<cr>', default_opts)
-remap('i', '<F9>', '<C-o> :set spell!<cr>', default_opts)
-remap('n', '<F10>', toggle_diff, default_opts)
+map('n', '<F1>', mapping_func_key_help)
+map('n', '<F2>', function() require("dapui").toggle() end)
+map('n', '<F3>', ':ToggleIndent<cr>')
+map('n', '<F4>', navigation_bar)
+map('n', '<F5>', ':exe "HighlightGroupsAddWord " . hg0 . " 1"<cr>')
+map('n', '\\<F5>', ':exe "HighlightGroupsClearGroup " . hg0 . " 1"<cr>')
+map('n', '<F6>', ':exe "HighlightGroupsAddWord " . hg1 . " 1"<cr>')
+map('n', '\\<F6>', ':exe "HighlightGroupsClearGroup " . hg1 . " 1"<cr>')
+map('n', '<F8>', ':call asyncrun#quickfix_toggle(8)<cr>')
+map('n', '<F9>', ':set spell!<cr>')
+map('i', '<F9>', '<C-o> :set spell!<cr>')
+map('n', '<F10>', toggle_diff)
 
 -- git
-remap('n', ']g', ':silent Gitsigns next_hunk<CR>', default_opts)
-remap('n', '[g', ':silent Gitsigns prev_hunk<CR>', default_opts)
-remap('o', 'ig', ':<C-U>Gitsigns select_hunk<CR>', default_opts)
-remap('x', 'ig', ':<C-U>Gitsigns select_hunk<CR>', default_opts)
+map('n', ']g', ':silent Gitsigns next_hunk<CR>')
+map('n', '[g', ':silent Gitsigns prev_hunk<CR>')
+map('o', 'ig', ':<C-U>Gitsigns select_hunk<CR>')
+map('x', 'ig', ':<C-U>Gitsigns select_hunk<CR>')
 
 -- tags: select when multiple matches
-remap('n', '<C-LeftMouse>', 'g<C-]>', default_opts)
-remap('n', '<C-]>', 'g<C-]>', default_opts)
-remap('n', '<C-w>]', '<C-w>g<C-]>', default_opts)
-remap('n', '<C-w><C-]>', '<C-w>g<C-]>', default_opts)
+map('n', '<C-LeftMouse>', 'g<C-]>')
+map('n', '<C-]>', 'g<C-]>')
+map('n', '<C-w>]', '<C-w>g<C-]>')
+map('n', '<C-w><C-]>', '<C-w>g<C-]>')
+
+-- quickfix location list
+map('n', ']c', ':silent! cnext<cr>')
+map('n', '[c', ':silent! cprev<cr>')
+map('n', ']l', ':silent! lnext<cr>')
+map('n', '[l', ':silent! lprev<cr>')
+map('n', '[t', function() next_sidebar_location(-1) end)
+map('n', ']t', function() next_sidebar_location(1) end)
+map('n', '[t', function() next_sidebar_location(-1) end)
 
 -- misc
-vim.keymap.set('n', ']t', function() next_location(1) end, {silent=true})
-vim.keymap.set('n', '[t', function() next_location(-1) end, {silent=true})
-
-remap('n', '\\r', ':RunCurrentBuffer<cr>', default_opts)
-remap('n', '\\t', ':RunAndTimeCurrentBuffer<cr>', default_opts)
-remap('v', '<LeftRelease>', '"*ygv', default_opts) -- copy/paste with mouse select
-remap('x', 'ga', '<Plug>(EasyAlign)', plug_opts)
-remap('n', 'ga', '<Plug>(EasyAlign)', plug_opts)
-remap('x', 'gm', ':<C-U>lua _G.TUtils.indentation_to_bullets("\'<", "\'>")<CR>', plug_opts)
-remap('n', 'gm', ':<C-U>set opfunc=v:lua.TUtils.indentation_to_bullets_opfunc<CR>g@', plug_opts)
-remap('n', '<C-S-Up>', '<Plug>(VM-Add-Cursor-Up)', plug_opts)
-remap('n', '<C-S-Down>', '<Plug>(VM-Add-Cursor-Down)', plug_opts)
-remap('n', 'dO', ':1,$+1diffget<cr>:diffupdate<cr>', default_opts)
-remap('n', 'dP', ':wincmd w<cr>:1,$+1diffget<cr>:wincmd w<cr>:diffupdate<cr>', default_opts)
-remap('t', '\\cd', 'nvim_server_cmd "cd $PWD" -i<cr>', default_opts)
+map('n', '\\r', ':RunCurrentBuffer<cr>')
+map('n', '\\t', ':RunAndTimeCurrentBuffer<cr>')
+map('v', '<LeftRelease>', '"*ygv') -- copy/paste with mouse select
+map('x', 'ga', '<Plug>(EasyAlign)', { remap = true })
+map('n', 'ga', '<Plug>(EasyAlign)', { remap = true })
+map('x', 'gm', ':<C-U>lua _G.TUtils.indentation_to_bullets("\'<", "\'>")<CR>', { remap = true })
+map('n', 'gm', ':<C-U>set opfunc=v:lua.TUtils.indentation_to_bullets_opfunc<CR>g@', { remap = true })
+map('n', '<C-S-Up>', '<Plug>(VM-Add-Cursor-Up)', { remap = true })
+map('n', '<C-S-Down>', '<Plug>(VM-Add-Cursor-Down)', { remap = true })
+map('n', 'dO', ':1,$+1diffget<cr>:diffupdate<cr>')
+map('n', 'dP', ':wincmd w<cr>:1,$+1diffget<cr>:wincmd w<cr>:diffupdate<cr>')
+map('t', '\\cd', 'nvim_server_cmd "cd $PWD" -i<cr>')
 
 -- LSP mappings
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '\\d', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
--- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+map('n', '\\d', vim.diagnostic.open_float)
+map('n', '[d', vim.diagnostic.goto_prev)
+map('n', ']d', vim.diagnostic.goto_next)
+-- remap('n', '<space>q', vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
@@ -183,15 +199,15 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', require'telescope.builtin'.lsp_definitions, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi',  require'telescope.builtin'.lsp_implementations, opts)
-    vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, opts)
-    -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '\\rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '\\ac', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', require'telescope.builtin'.lsp_references, opts)
-    vim.keymap.set('n', '\\f', _G.LUtils.format, opts)
+    map('n', 'gD', vim.lsp.buf.declaration, opts)
+    map('n', 'gd', require 'telescope.builtin'.lsp_definitions, opts)
+    map('n', 'K', vim.lsp.buf.hover, opts)
+    map('n', 'gi', require 'telescope.builtin'.lsp_implementations, opts)
+    map('n', '<C-s>', vim.lsp.buf.signature_help, opts)
+    -- remap('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    map('n', '\\rn', vim.lsp.buf.rename, opts)
+    map({ 'n', 'v' }, '\\ac', vim.lsp.buf.code_action, opts)
+    map('n', 'gr', require 'telescope.builtin'.lsp_references, opts)
+    map('n', '\\f', _G.LUtils.format, opts)
   end,
 })
