5a29863893efa9fd37522dc449edafd6e46bfc52
antoinemadec <ajamadec@gmail.com>
2024-05-09 16:32:55 +0200

    feat(nvim): no preview for buffer search

--- .config/nvim/lua/avim/config/which_key.lua
+++ .config/nvim/lua/avim/config/which_key.lua
@@ -76,11 +76,11 @@ wk.setup({
 wk.register(
   {
     -- top-level mappings
-    ['/'] = {'<cmd>Telescope current_buffer_fuzzy_find<cr>', 'search in file'},
-    [';'] = {'<cmd>Telescope commands<cr>',                  'commands'},
-    ['y'] = {'<cmd>Telescope neoclip<cr>',                   'clipboard manager'},
-    ['h'] = {'<cmd>Telescope help_tags<cr>',                 'vim help'},
-    ['m'] = {'<cmd>MarkdownPreviewToggle<cr>',               'markdown preview'},
+    ['/'] = {'<cmd>Telescope current_buffer_fuzzy_find previewer=false<cr>', 'search in file'},
+    [';'] = {'<cmd>Telescope commands<cr>',                                  'commands'},
+    ['y'] = {'<cmd>Telescope neoclip<cr>',                                   'clipboard manager'},
+    ['h'] = {'<cmd>Telescope help_tags<cr>',                                 'vim help'},
+    ['m'] = {'<cmd>MarkdownPreviewToggle<cr>',                               'markdown preview'},
     -- nvim UI
     ['u'] = {
       name = 'change UI',
