5efdc5899e16ace0673e96f5db77fc62da00cc5e
antoinemadec <ajamadec@gmail.com>
2024-10-16 20:58:34 +0200

    feat(nvim): nicer command window

--- .config/nvim/lua/avim/config/cmdline.lua
+++ .config/nvim/lua/avim/config/cmdline.lua
@@ -18,4 +18,25 @@ vim.api.nvim_create_autocmd("CmdlineLeave", {
   end,
 })
 
+-- don't use treesitter <CR> remap in the command window
+vim.api.nvim_create_autocmd("CmdwinEnter", {
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', '<CR>', '<CR>' , opts)
+  end,
+})
+
+-- set the syntax to get highlighting in the command window (q: and friends)
+vim.api.nvim_create_autocmd('CmdwinEnter', {
+  pattern = '[:>]',
+  desc = 'If the treesitter vim parser is installed, set the syntax again to get highlighting in the command window',
+  group = vim.api.nvim_create_augroup('cmdwin_syntax', {}),
+  callback = function ()
+    local is_loadable, _ = pcall(vim.treesitter.language.add, 'vim')
+    if is_loadable then
+      vim.cmd('set syntax=vim')
+    end
+  end
+})
+
 require("cmdline-hl").setup()
