74db1e7345bf853ffe43a252511da6af32b329bf
antoinemadec <ajamadec@gmail.com>
2025-01-10 12:15:21 +0100

    fix: missing gitlab-ls dependency

--- .config/nvim/lua/avim/plugins.lua
+++ .config/nvim/lua/avim/plugins.lua
@@ -227,7 +227,7 @@ require("lazy").setup({
   { -- gitlab ls
     "jrmsgr/gitlab-ls",
     cond = (os.getenv("GITLAB_API_TOKEN") and os.getenv("GITLAB_URL") and os.getenv("GITLAB_PROJECTS")) ~= nil,
-    dependencies = { "neovim/nvim-lspconfig" },
+    dependencies = { "neovim/nvim-lspconfig", "hrsh7th/nvim-cmp" },
     opts = { -- Plugin's config
       max_txt_len = 20,
       open_icon = "",
