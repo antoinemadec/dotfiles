7756df5a985183e597faff20f0887994fdbcf743
antoinemadec <ajamadec@gmail.com>
2025-08-27 10:13:18 +0200

    feat: update copilot mapping

--- .config/nvim/lua/avim/config/copilot.lua
+++ .config/nvim/lua/avim/config/copilot.lua
@@ -1,7 +1,8 @@
-vim.cmd([[
-imap <silent><script><expr> <M-Tab> copilot#Accept("\<CR>")
-let g:copilot_no_tab_map = v:true
-]])
+vim.keymap.set('i', '<C-CR>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
 
 vim.api.nvim_create_autocmd('BufEnter', {
   pattern = { "*" },
