57cd8861e97cc40f39bb70f55bba50be7a261a65
antoinemadec <ajamadec@gmail.com>
2026-01-07 10:31:58 +0100

    fix(nvim): bash treesitter

--- .config/nvim/lua/avim/config/treesitter.lua
+++ .config/nvim/lua/avim/config/treesitter.lua
@@ -26,8 +26,18 @@ require('nvim-treesitter').install(ts_languages)
 
 vim.treesitter.language.register('bash', { 'sh' })
 
+local ts_filetypes = {}
+for _, lang in ipairs(ts_languages) do
+  local fts = vim.treesitter.language.get_filetypes(lang)
+  for _, ft in ipairs(fts) do
+    if not vim.tbl_contains(ts_filetypes, ft) then
+      table.insert(ts_filetypes, ft)
+    end
+  end
+end
+
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = ts_languages,
+  pattern = ts_filetypes,
   callback = function(ev)
     if vim.api.nvim_buf_line_count(ev.buf) > vim.g.large_file_cutoff then
       return
