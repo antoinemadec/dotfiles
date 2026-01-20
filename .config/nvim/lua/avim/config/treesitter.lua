781c7c82daf236b16aa82a1d7c3f33e331a44d4c
antoinemadec <ajamadec@gmail.com>
2026-01-20 09:46:08 +0100

    feat(nvim): add old treesitter fallback

--- .config/nvim/lua/avim/config/treesitter.lua
+++ .config/nvim/lua/avim/config/treesitter.lua
@@ -1,11 +1,3 @@
-vim.filetype.add({
-  extension = {
-    v = 'systemverilog',
-    svh = 'systemverilog',
-    sv = 'systemverilog',
-  }
-})
-
 local ts_languages = {
   'bash',
   'cpp',
@@ -22,26 +14,56 @@ local ts_languages = {
   'vimdoc',
 }
 
-require('nvim-treesitter').install(ts_languages)
+vim.filetype.add({
+  extension = {
+    v = 'systemverilog',
+    svh = 'systemverilog',
+    sv = 'systemverilog',
+  }
+})
 
 vim.treesitter.language.register('bash', { 'sh' })
 
-local ts_filetypes = {}
-for _, lang in ipairs(ts_languages) do
-  local fts = vim.treesitter.language.get_filetypes(lang)
-  for _, ft in ipairs(fts) do
-    if not vim.tbl_contains(ts_filetypes, ft) then
-      table.insert(ts_filetypes, ft)
+local is_old_nvim_treesitter, old_nvim_treesitter = pcall(require, 'nvim-treesitter.configs')
+
+if is_old_nvim_treesitter then
+  local function disable(lang, bufnr)
+    return vim.api.nvim_buf_line_count(bufnr) > vim.g.large_file_cutoff
+  end
+
+  ---@diagnostic disable: missing-fields
+  old_nvim_treesitter.setup({
+    ensure_installed = ts_languages,
+    highlight = {
+      enable = true,
+      disable = disable,
+      additional_vim_regex_highlighting = false,
+    },
+    incremental_selection = {
+      enable = false,
+      disable = disable,
+    },
+  })
+else
+  require('nvim-treesitter').install(ts_languages)
+
+  local ts_filetypes = {}
+  for _, lang in ipairs(ts_languages) do
+    local fts = vim.treesitter.language.get_filetypes(lang)
+    for _, ft in ipairs(fts) do
+      if not vim.tbl_contains(ts_filetypes, ft) then
+        table.insert(ts_filetypes, ft)
+      end
     end
   end
-end
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = ts_filetypes,
-  callback = function(ev)
-    if vim.api.nvim_buf_line_count(ev.buf) > vim.g.large_file_cutoff then
-      return
-    end
-    vim.treesitter.start()
-  end,
-})
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = ts_filetypes,
+    callback = function(ev)
+      if vim.api.nvim_buf_line_count(ev.buf) > vim.g.large_file_cutoff then
+        return
+      end
+      vim.treesitter.start()
+    end,
+  })
+end
